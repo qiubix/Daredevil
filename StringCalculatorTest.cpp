9df443b596ef6395930945d780e31de2be6ad60f
qiubix <qiubix@gmail.com>
2015-08-18 00:59:13 +0200

    Return one for single char 1

--- StringCalculatorTest.cpp
+++ StringCalculatorTest.cpp
@@ -3,9 +3,15 @@ using ::testing::Eq;
 using namespace std;
 
 int add(string numbers) {
+  if (numbers == "1")
+    return 1;
   return 0;
 }
 
 TEST(StringCalculator, ReturnsZeroForAnEmptyString) {
   ASSERT_THAT(add(""), Eq(0));
+}
+
+TEST(StringCalculator, ReturnsOneForSingleOne) {
+  ASSERT_THAT(add("1"), Eq(1));
 }
