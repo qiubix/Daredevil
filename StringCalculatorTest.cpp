17406a295e6a12622738a67df396f97cc46e26ad
qiubix <qiubix@gmail.com>
2015-08-18 03:25:58 +0200

    Enhance test for one number,
    return zero while summing up two zeros

--- StringCalculatorTest.cpp
+++ StringCalculatorTest.cpp
@@ -20,9 +20,13 @@ TEST(StringCalculator, ReturnsOneForSingleOne) {
   ASSERT_THAT(add("1"), Eq(1));
 }
 
-TEST(StringCalculator, ReturnsSameNumberForStringWithOneCharacter) {
+TEST(StringCalculator, ReturnsSameNumberForStringWithOneNumber) {
   ASSERT_THAT(add("3"), Eq(3));
+  ASSERT_THAT(add("15"), Eq(15));
+  ASSERT_THAT(add("1423"), Eq(1423));
 }
 
-
+TEST(StringCalculator, ReturnsZeroWhileSummingUpTwoZeros) {
+  ASSERT_THAT(add("0,0"), Eq(1));
+}
 
