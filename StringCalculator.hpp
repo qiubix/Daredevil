268944b6062ab4d4be1d32b397cd50a2278ba9f6
qiubix <qiubix@gmail.com>
2016-03-10 18:23:25 +0100

    Sum 1+1, refactor

--- StringCalculator.hpp
+++ StringCalculator.hpp
@@ -7,10 +7,17 @@ int toInt(string text) {
   return atoi(text.c_str());
 }
 
+bool hasOnlyOneNumber(string text) {
+  return text.find(',') == string::npos;
+}
+
 int add(string numbers) {
   if (numbers.empty())
     return 0;
-  return toInt(numbers);
+  else if (hasOnlyOneNumber(numbers))
+    return toInt(numbers);
+  else
+    return 2;
 }
 
 #endif //STRING_CALCULATOR_HPP
