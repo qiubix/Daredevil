e1afe1a291985661a2012603eb10cc2ae5bee794
qiubix <qiubix@gmail.com>
2016-03-11 01:20:12 +0100

    Refactor: head and tail

--- StringCalculator.hpp
+++ StringCalculator.hpp
@@ -11,14 +11,25 @@ bool hasOnlyOneNumber(string text) {
   return text.find(',') == string::npos;
 }
 
+string head(string text) {
+  auto delimiterPosition = text.find(',');
+  return text.substr(0, delimiterPosition);
+}
+
+string tail(string text) {
+  auto delimiterPosition = text.find(',');
+  return text.substr(delimiterPosition + 1);
+}
+
 int add(string numbers) {
   if (numbers.empty())
     return 0;
   else if (hasOnlyOneNumber(numbers))
     return toInt(numbers);
   else {
-    auto delimiterPosition = numbers.find(',');
-    return toInt(numbers.substr(0, delimiterPosition)) + toInt(numbers.substr(delimiterPosition+1));
+    auto firstNumber = head(numbers);
+    auto secondNumber = tail(numbers);
+    return toInt(firstNumber) + toInt(secondNumber);
   }
 }
 
--- StringCalculatorTest.cpp
+++ StringCalculatorTest.cpp
@@ -16,5 +16,5 @@ TEST(StringCalculator, ReturnsSameNumberForASingleNumber) {
 TEST(StringCalculator, ReturnsSumForTwoNumbersWithComa) {
   ASSERT_THAT(add("1,1"), Eq(2));
   ASSERT_THAT(add("2,1"), Eq(3));
-//  ASSERT_THAT(add("12,11"), Eq(23));
+  ASSERT_THAT(add("12,11"), Eq(23));
 }
