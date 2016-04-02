5086b4deba1d198026455f7003092a48088c1b19
qiubix <qiubix@gmail.com>
2016-04-02 22:16:41 +0200

    Add two numbers with coma

--- StringCalculator.hpp
+++ StringCalculator.hpp
@@ -3,11 +3,31 @@
 
 using namespace std;
 
+int toInt(string text) {
+  return atoi(text.c_str());
+}
+
+bool hasOnlyOneNumber(string text) {
+  return text.find(',') == string::npos;
+}
+
+string head(string text, size_t delimiterPosition) {
+  return text.substr(0, delimiterPosition);
+}
+
+string tail(string text, size_t delimiterPosition) {
+  return text.substr(delimiterPosition + 1);
+}
+
 int add(string numbers) {
   if (numbers.empty())
     return 0;
-  else
-    return atoi(numbers.c_str());
+  else if (hasOnlyOneNumber(numbers))
+    return toInt(numbers);
+  else {
+    auto delimiterPosition = numbers.find(',');
+    return toInt(head(numbers, delimiterPosition)) + toInt(tail(numbers, delimiterPosition));
+  }
 }
 
 #endif //STRING_CALCULATOR_HPP
--- StringCalculatorTest.cpp
+++ StringCalculatorTest.cpp
@@ -12,3 +12,9 @@ TEST(StringCalculator, ReturnsSameNumberForASingleNumber) {
   ASSERT_THAT(add("2"), Eq(2));
   ASSERT_THAT(add("13"), Eq(13));
 }
+
+TEST(StringCalculator, ReturnsSumForTwoNumbersWithComa) {
+  ASSERT_THAT(add("1,1"), Eq(2));
+  ASSERT_THAT(add("2,1"), Eq(3));
+  ASSERT_THAT(add("12,11"), Eq(23));
+}
